915869d6bd03124a77e6d83d1cf9fc02a4c6eb1f
JhonatanPereira <jhonisilva@outlook.com.br>
2018-04-03 03:42:32 -0300

    Upload do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Realtime Chat
+
+Chat em tempo real com socket.io desenvolvido para estudo
---
+++ index.js
@@ -0,0 +1,43 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var sanitizeHtml = require('sanitize-html');
+
+var mensagens = [];
+
+var sanitize = function(data) {
+    return sanitizeHtml(data, {
+        allowedTags: [ 'b', 'i', 'em', 'strong', 'a' ],
+        allowedAttributes: {
+            'a': [ 'href' ]
+        },
+        allowedIframeHostnames: ['www.youtube.com']
+    });
+};
+
+var sendOldMessages = function() {
+    mensagens.forEach(element => {
+        io.emit('chat message', element);
+    });
+}
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    sendOldMessages();
+    socket.on('chat message', function(msg){
+        msg.mensagem = sanitize(msg.mensagem);
+        msg.nick = sanitize(msg.nick);
+        if (msg.mensagem && msg.nick) {
+            io.emit('chat message', msg);
+            mensagens.push(msg);
+        }
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
